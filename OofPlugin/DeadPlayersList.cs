96cd69a76a73a778b18f9eb3989bc68083db9df7
HeyImKiri <emailingkiriakashi@gmail.com>
2025-12-17 21:45:33 -0600

    API 14

--- OofPlugin/DeadPlayersList.cs
+++ OofPlugin/DeadPlayersList.cs
@@ -4,43 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 
-namespace OofPlugin
-{
-    public class DeadPlayersList
-    {
-        public class DeadPlayer
-        {
-            public uint PlayerId;
-            public bool DidPlayOof { get; set; } = false;
-            public Vector3 Distance = Vector3.Zero;
-        }
-
-        public List<DeadPlayer> DeadPlayers { get; set; } = new();
-
-        private void AddRemoveDeadPlayer(uint currentHp, uint entityId, Vector3 pos, bool condition = true)
-        {
-            if (!condition) return;
-
-            if (currentHp == 0 && !DeadPlayers.Any(x => x.PlayerId == entityId))
-            {
-                DeadPlayers.Add(new DeadPlayer { PlayerId = entityId, Distance = pos });
-            }
-            else if (currentHp != 0 && DeadPlayers.Any(x => x.PlayerId == entityId))
-            {
-                DeadPlayers.RemoveAll(x => x.PlayerId == entityId);
-            }
-        }
-
-        public void AddRemoveDeadPlayer(IPlayerCharacter character)
-        {
-            if (character == null) return;
-            AddRemoveDeadPlayer(character.CurrentHp, character.EntityId, character.Position, true);
-        }
-
-        public void AddRemoveDeadPlayer(IPartyMember character, bool condition = true)
-        {
-            if (character == null) return;
-            AddRemoveDeadPlayer(character.CurrentHP, character.EntityId, character.Position, condition);
-        }
+namespace OofPlugin {
+  public class DeadPlayersList {
+    public class DeadPlayer {
+      public uint PlayerId;
+      public bool DidPlayOof { get; set; } = false;
+      public Vector3 Distance = Vector3.Zero;
     }
+
+    public List<DeadPlayer> DeadPlayers { get; set; } = new();
+
+    private void AddRemoveDeadPlayer(uint currentHp, uint entityId, Vector3 pos,
+                                     bool condition = true) {
+      if (!condition)
+        return;
+
+      if (currentHp == 0 && !DeadPlayers.Any(x => x.PlayerId == entityId)) {
+        DeadPlayers.Add(new DeadPlayer { PlayerId = entityId, Distance = pos });
+      } else if (currentHp != 0 &&
+                 DeadPlayers.Any(x => x.PlayerId == entityId)) {
+        DeadPlayers.RemoveAll(x => x.PlayerId == entityId);
+      }
+    }
+
+    public void AddRemoveDeadPlayer(IPlayerCharacter character) {
+      if (character == null)
+        return;
+      AddRemoveDeadPlayer(character.CurrentHp, character.EntityId,
+                          character.Position, true);
+    }
+
+    public void AddRemoveDeadPlayer(IPartyMember character,
+                                    bool condition = true) {
+      if (character == null)
+        return;
+      AddRemoveDeadPlayer(character.CurrentHP, character.EntityId,
+                          character.Position, condition);
+    }
+  }
 }
