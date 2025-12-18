4f77077bf28fa21e60e8bc07d21ee09a675cd36a
HeyImKiri <emailingkiriakashi@gmail.com>
2025-12-17 21:45:48 -0600

    API 14

--- OofPlugin/OofPlugin.cs
+++ OofPlugin/OofPlugin.cs
@@ -9,194 +9,199 @@ using System;
 using Dalamud.IoC;
 using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Objects.SubKinds;
+using System.Linq;
 
-//shoutout anna clemens
+// shoutout anna clemens
 
 namespace OofPlugin;
 
-public sealed class OofPlugin : IDalamudPlugin
-{
-    public string Name => "OOF";
+public sealed class OofPlugin : IDalamudPlugin {
+  public string Name => "OOF";
 
-    private const string oofCommand = "/oof";
-    private const string oofSettings = "/oofsettings";
-    private const string oofVideo = "/oofvideo";
-    
-    [PluginService] private static IObjectTable ObjectTable { get; set; } = null!;
+  private const string oofCommand = "/oof";
+  private const string oofSettings = "/oofsettings";
+  private const string oofVideo = "/oofvideo";
 
-    public readonly WindowSystem WindowSystem = new("OofPlugin");
-    public Configuration Configuration { get; init; }
-    private ConfigWindow ConfigWindow { get; init; }
-    internal DeadPlayersList DeadPlayersList { get; init; }
-    internal SoundManager SoundManager { get; init; }
+  [PluginService]
+  internal static IObjectTable ObjectTable { get; private set; } = null!;
 
-    // i love global variables!!!! the more global the more globaly it gets
+  public readonly WindowSystem WindowSystem = new("OofPlugin");
+  public Configuration Configuration { get; init; }
+  private ConfigWindow ConfigWindow { get; init; }
+  internal DeadPlayersList DeadPlayersList { get; init; }
+  internal SoundManager SoundManager { get; init; }
 
-    //check for fall
-    private float prevPos { get; set; } = 0;
-    private float prevVel { get; set; } = 0;
-    private float distJump { get; set; } = 0;
-    private bool wasJumping { get; set; } = false;
+  // i love global variables!!!! the more global the more globaly it gets
 
+  // check for fall
+  private float prevPos { get; set; } = 0;
+  private float prevVel { get; set; } = 0;
+  private float distJump { get; set; } = 0;
+  private bool wasJumping { get; set; } = false;
 
-    public OofPlugin(IDalamudPluginInterface pluginInterface)
+  public OofPlugin(IDalamudPluginInterface pluginInterface) {
+    Dalamud.Initialize(pluginInterface);
+
+    Configuration = pluginInterface.GetPluginConfig() as Configuration ??
+                    new Configuration();
+    Configuration.Initialize(pluginInterface);
+
+    DeadPlayersList = new DeadPlayersList();
+    SoundManager = new SoundManager(this);
+
+    ConfigWindow = new ConfigWindow(this);
+    WindowSystem.AddWindow(ConfigWindow);
+
+    Dalamud.PluginInterface.UiBuilder.Draw += DrawUI;
+    Dalamud.PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
+    Dalamud.Framework.Update += FrameworkOnUpdate;
+
+    Dalamud.CommandManager.AddHandler(
+        oofCommand,
+        new CommandInfo(OnCommand) { HelpMessage = "play oof sound" });
+    Dalamud.CommandManager.AddHandler(
+        oofSettings,
+        new CommandInfo(OnCommand) { HelpMessage = "change oof settings" });
+    Dalamud.CommandManager.AddHandler(oofVideo, new CommandInfo(OnCommand) {
+      HelpMessage = "open Hbomberguy video on OOF.mp3"
+    });
+  }
+
+  private void DrawUI() => WindowSystem.Draw();
+  public void ToggleConfigUI() => ConfigWindow.Toggle();
+  private void OnCommand(string command, string args) {
+    if (command == oofCommand)
+      SoundManager.Play(SoundManager.CancelToken.Token);
+    if (command == oofSettings)
+      ToggleConfigUI();
+    if (command == oofVideo)
+      OpenVideo();
+  }
+
+  private void FrameworkOnUpdate(IFramework framework) {
+    var localPlayer = ObjectTable.LocalPlayer as IPlayerCharacter;
+    if (localPlayer is null)
+      return;
+
+    if (Dalamud.Condition[ConditionFlag.BetweenAreas] ||
+        Dalamud.Condition[ConditionFlag.BetweenAreas51] ||
+        Dalamud.Condition[ConditionFlag.BeingMoved])
+      return;
+
+    if (Dalamud.Condition[ConditionFlag.WatchingCutscene] ||
+        Dalamud.Condition[ConditionFlag.WatchingCutscene78])
+      return;
+
+    try {
+      if (Configuration.OofOnFall)
+        CheckFallen(localPlayer);
+      if (Configuration.OofOnDeath)
+        CheckDeath(localPlayer);
+    } catch (Exception e) {
+      Dalamud.Log.Error($"failed to check for oof condition: {e}");
+    }
+  }
+
+  /// <summary>
+  /// check if player has died during alliance, party, and self.
+  /// this may be the worst if statement chain i have made
+  /// </summary>
+
+  private void CheckDeath(IPlayerCharacter localPlayer) {
+    if (!Configuration.OofOnDeathBattle &&
+        Dalamud.Condition[ConditionFlag.InCombat])
+      return;
+
+    // Always track self death if enabled (even while in party)
+    if (Configuration.OofOnDeathSelf)
+      DeadPlayersList.AddRemoveDeadPlayer(localPlayer);
+
+    if (Dalamud.PartyList != null && Dalamud.PartyList.Any()) {
+      // Alliance members
+      if (Configuration.OofOnDeathAlliance && Dalamud.PartyList.Length == 8 &&
+          Dalamud.PartyList.GetAllianceMemberAddress(0) != IntPtr.Zero) {
+        try {
+          for (int i = 0; i < 16; i++) {
+            var addr = Dalamud.PartyList.GetAllianceMemberAddress(i);
+            if (addr == IntPtr.Zero)
+              throw new NullReferenceException(
+                  "alliance member address is null");
+
+            var allianceMember =
+                Dalamud.PartyList.CreateAllianceMemberReference(addr) ??
+                throw new NullReferenceException("alliance reference is null");
+
+            DeadPlayersList.AddRemoveDeadPlayer(allianceMember);
+          }
+        } catch (Exception e) {
+          Dalamud.Log.Error("failed alliance check", e.Message);
+        }
+      }
+
+      // Party members (including yourself if PartyList contains you)
+      if (Configuration.OofOnDeathParty) {
+        foreach (var member in Dalamud.PartyList) {
+          DeadPlayersList.AddRemoveDeadPlayer(
+              member,
+              member.Territory.RowId == Dalamud.ClientState.TerritoryType);
+        }
+      }
+    }
+  }
+
+  /// <summary>
+  /// check if player has taken fall damage (brute force way)
+  /// </summary>
+  private void CheckFallen(IPlayerCharacter localPlayer) {
+    if (!Configuration.OofOnFallBattle &&
+        Dalamud.Condition[ConditionFlag.InCombat])
+      return;
+    if (!Configuration.OofOnFallMounted &&
+        (Dalamud.Condition[ConditionFlag.Mounted] ||
+         Dalamud.Condition[ConditionFlag.RidingPillion]))
+      return;
+
+    var isJumping = Dalamud.Condition[ConditionFlag.Jumping];
+    var pos = localPlayer.Position.Y;
+    var velocity = prevPos - pos;
+
+    if (isJumping && !wasJumping) {
+      if (prevVel < 0.17)
+        distJump = pos;                   // started falling
+    } else if (wasJumping && !isJumping)  // stopped falling
     {
-        Dalamud.Initialize(pluginInterface);
-
-        Configuration = pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-        Configuration.Initialize(pluginInterface);
-
-        DeadPlayersList = new DeadPlayersList();
-        SoundManager = new SoundManager(this);
-
-
-        ConfigWindow = new ConfigWindow(this);
-        WindowSystem.AddWindow(ConfigWindow);
-
-        Dalamud.PluginInterface.UiBuilder.Draw += DrawUI;
-        Dalamud.PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
-        Dalamud.Framework.Update += FrameworkOnUpdate;
-
-        Dalamud.CommandManager.AddHandler(oofCommand, new CommandInfo(OnCommand)
-        {
-            HelpMessage = "play oof sound"
-        });
-        Dalamud.CommandManager.AddHandler(oofSettings, new CommandInfo(OnCommand)
-        {
-            HelpMessage = "change oof settings"
-        });
-        Dalamud.CommandManager.AddHandler(oofVideo, new CommandInfo(OnCommand)
-        {
-            HelpMessage = "open Hbomberguy video on OOF.mp3"
-        });
-
+      if (distJump - pos > 9.60)
+        SoundManager.Play(
+            SoundManager.CancelToken
+                .Token);  // fell enough to take damage // i guessed and checked
+                          // this distance value btw
     }
 
-    private void DrawUI() => WindowSystem.Draw();
-    public void ToggleConfigUI() => ConfigWindow.Toggle();
-    private void OnCommand(string command, string args)
-    {
-        if (command == oofCommand) SoundManager.Play(SoundManager.CancelToken.Token);
-        if (command == oofSettings) ToggleConfigUI();
-        if (command == oofVideo) OpenVideo();
+    // set position for next timestep
+    prevPos = pos;
+    prevVel = velocity;
+    wasJumping = isJumping;
+  }
 
-    }
+  /// <summary>
+  /// open the hbomberguy video on oof
+  /// </summary>
+  public static void OpenVideo() {
+    Util.OpenLink("https://www.youtube.com/watch?v=0twDETh6QaI");
+  }
 
-    private void FrameworkOnUpdate(IFramework framework)
-    {
-        var localPlayer = ObjectTable.LocalPlayer as IPlayerCharacter;
-        if (localPlayer is null)
-        return;
-        // dont run btwn moving areas          
-        if (Dalamud.Condition[ConditionFlag.BetweenAreas] || Dalamud.Condition[ConditionFlag.BetweenAreas51] || Dalamud.Condition[ConditionFlag.BeingMoved]) return;
-        if (Dalamud.Condition[ConditionFlag.WatchingCutscene] || Dalamud.Condition[ConditionFlag.WatchingCutscene78]) return;
+  /// <summary>
+  /// dispose
+  /// </summary>
+  public void Dispose() {
+    WindowSystem.RemoveAllWindows();
+    ConfigWindow.Dispose();
+    SoundManager.Dispose();
 
+    Dalamud.CommandManager.RemoveHandler(oofCommand);
+    Dalamud.CommandManager.RemoveHandler(oofSettings);
+    Dalamud.CommandManager.RemoveHandler(oofVideo);
 
-        try
-        {
-            if (Configuration.OofOnFall) CheckFallen(localPlayer);
-            if (Configuration.OofOnDeath) CheckDeath(localPlayer);
-        }
-        catch (Exception e)
-        {
-            Dalamud.Log.Error("failed to check for oof condition:", e.Message);
-        }
-    }
-
-    /// <summary>
-    /// check if player has died during alliance, party, and self.
-    /// this may be the worst if statement chain i have made
-    /// </summary>
-    private void CheckDeath(IPlayerCharacter localPlayer)
-    {
-
-        if (!Configuration.OofOnDeathBattle && Dalamud.Condition[ConditionFlag.InCombat]) return;
-
-        // if not in party
-        if (Configuration.OofOnDeathSelf && (Dalamud.PartyList == null || Dalamud.PartyList.Length == 0))
-        {
-            DeadPlayersList.AddRemoveDeadPlayer(localPlayer);
-            return;
-        }
-        // if in alliance
-        if (Configuration.OofOnDeathAlliance && Dalamud.PartyList.Length == 8 && Dalamud.PartyList.GetAllianceMemberAddress(0) != IntPtr.Zero) // the worst "is alliance" check
-        {
-            try
-            {
-                for (int i = 0; i < 16; i++)
-                {
-                    var allianceMemberAddress = Dalamud.PartyList.GetAllianceMemberAddress(i);
-                    if (allianceMemberAddress == IntPtr.Zero) throw new NullReferenceException("allience member address is null");
-
-                    var allianceMember = Dalamud.PartyList.CreateAllianceMemberReference(allianceMemberAddress) ?? throw new NullReferenceException("allience reference is null");
-                    DeadPlayersList.AddRemoveDeadPlayer(allianceMember);
-                }
-            }
-            catch (Exception e)
-            {
-                Dalamud.Log.Error("failed alliance check", e.Message);
-            }
-        }
-        //if in party
-        if (Configuration.OofOnDeathParty)
-        {
-            foreach (var member in Dalamud.PartyList)
-            {
-                DeadPlayersList.AddRemoveDeadPlayer(member, member.Territory.RowId == Dalamud.ClientState.TerritoryType);
-            }
-        }
-    }
-
-    /// <summary>
-    /// check if player has taken fall damage (brute force way)
-    /// </summary>
-    private void CheckFallen(IPlayerCharacter localPlayer)
-    {
-        if (!Configuration.OofOnFallBattle && Dalamud.Condition[ConditionFlag.InCombat]) return;
-        if (!Configuration.OofOnFallMounted && (Dalamud.Condition[ConditionFlag.Mounted] || Dalamud.Condition[ConditionFlag.RidingPillion])) return;
-
-        var isJumping = Dalamud.Condition[ConditionFlag.Jumping];
-        var pos = localPlayer.Position.Y;
-        var velocity = prevPos - pos;
-
-        if (isJumping && !wasJumping)
-        {
-            if (prevVel < 0.17) distJump = pos; //started falling
-        }
-        else if (wasJumping && !isJumping)  // stopped falling
-        {
-            if (distJump - pos > 9.60) SoundManager.Play(SoundManager.CancelToken.Token); // fell enough to take damage // i guessed and checked this distance value btw
-        }
-
-        // set position for next timestep
-        prevPos = pos;
-        prevVel = velocity;
-        wasJumping = isJumping;
-    }
-
-
-    /// <summary>
-    /// open the hbomberguy video on oof
-    /// </summary>
-    public static void OpenVideo()
-    {
-        Util.OpenLink("https://www.youtube.com/watch?v=0twDETh6QaI");
-    }
-
-    /// <summary>
-    /// dispose
-    /// </summary>
-    public void Dispose()
-    {
-        WindowSystem.RemoveAllWindows();
-        ConfigWindow.Dispose();
-        SoundManager.Dispose();
-
-        Dalamud.CommandManager.RemoveHandler(oofCommand);
-        Dalamud.CommandManager.RemoveHandler(oofSettings);
-        Dalamud.CommandManager.RemoveHandler(oofVideo);
-
-        Dalamud.Framework.Update -= FrameworkOnUpdate;
-
-    }
+    Dalamud.Framework.Update -= FrameworkOnUpdate;
+  }
 }
